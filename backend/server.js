6f1e4e09e45fc590383263500f3eee48cca9ee4a
Aravinth_C_M <aravinth.cm@aarvihsolutions.com>
2026-04-25 00:45:39 +0530

    cors ready

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "http://localhost:5174",
-    ],
+    origin: ["http://localhost:5173", "https://code-secure-ai.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
